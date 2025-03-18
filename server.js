8ce8348a9a4783814e6601d5b416d44699c1fb03
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2025-03-18 13:04:43 +0200

    Implemented CRUDL functions for the Bug API, enabling Create, Read, Update, Delete, and List operations.

--- server.js
+++ server.js
@@ -1,10 +1,68 @@
 import express from 'express'
-
+import { bugService } from './services/bug.service.js'
 const app = express()
 
+app.use(express.static('public'))
+
+//* Express Routing:
+
+//* Read
+app.get('/api/bug', (req, res) => {
+  bugService
+    .query()
+    .then((bugs) => res.send(bugs))
+    .catch((err) => {
+      console.log('Cannot get bugs', err)
+      res.status(500).send('Cannot load bugs')
+    })
+})
+
+//* Create/Edit
+app.get('/api/bug/save', (req, res) => {
+  const bugToSave = {
+    _id: req.query._id,
+    title: req.query.title,
+    description: req.query.description,
+    severity: +req.query.severity,
+    createdAt: +req.query.createdAt,
+  }
+
+  bugService
+    .save(bugToSave)
+    .then((bug) => res.send(bug))
+    .catch((err) => {
+      console.log('Cannot save bug', err)
+      res.status(500).send('Cannot save bug')
+    })
+})
+
+//* Get/Read by id
+app.get('/api/bug/:bugId', (req, res) => {
+  const { bugId } = req.params
+  bugService
+    .getById(bugId)
+    .then((bug) => res.send(bug))
+    .catch((err) => {
+      console.log('Cannot get bug', err)
+      res.status(500).send('Cannot load bug')
+    })
+})
+
+//* Remove/Delete
+app.get('/api/bug/:bugId/remove', (req, res) => {
+  const { bugId } = req.params
+  bugService
+    .remove(bugId)
+    .then(() => res.send('bug Removed'))
+    .catch((err) => {
+      console.log('Cannot remove bug', err)
+      res.status(500).send('Cannot remove bug')
+    })
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello there')
+  res.send('Hello And Welcome')
 })
 
 const port = 3030
-app.listen(port, () => console.log(`Server ready at port ${port}`))
+app.listen(port, () => console.log(`Server listening on port http://127.0.0.1:${port}/`))
---
+++ services/bug.service.js
@@ -0,0 +1,52 @@
+import { utilService } from './util.service.js'
+import fs from 'fs'
+
+const bugs = utilService.readJsonFile('./data/bug.json')
+
+export const bugService = {
+  query,
+  getById,
+  remove,
+  save,
+}
+
+function query() {
+  return Promise.resolve(bugs)
+}
+
+function getById(bugId) {
+  const bug = bugs.find((bug) => bug._id === bugId)
+  if (!bug) return Promise.reject('Cannot find bug - ' + bugId)
+  return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+  const bugIdx = bugs.findIndex((bug) => bug._id === bugId)
+  if (bugIdx === -1) return Promise.reject('Cannot remove bug - ' + bugId)
+  bugs.splice(bugIdx, 1)
+  return _savebugsToFile()
+}
+
+function save(bugToSave) {
+  if (bugToSave._id) {
+    const bugIdx = bugs.findIndex((bug) => bug._id === bugToSave._id)
+    bugs[bugIdx] = bugToSave
+  } else {
+    bugToSave._id = utilService.makeId()
+    bugs.unshift(bugToSave)
+  }
+
+  return _savebugsToFile().then(() => bugToSave)
+}
+
+function _savebugsToFile() {
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify(bugs, null, 4)
+    fs.writeFile('data/bug.json', data, (err) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve()
+    })
+  })
+}

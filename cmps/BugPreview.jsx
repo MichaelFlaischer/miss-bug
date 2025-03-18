5d5ce2b053fae8afdf4b6311dd3dfa2fd2eb79da
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2025-03-18 13:39:34 +0200

    Updated the frontend to support displaying bug descriptions alongside other bug details.

--- cmps/BugPreview.jsx
+++ cmps/BugPreview.jsx
@@ -1,6 +1,13 @@
-export function BugPreview({bug}) {
-    return <article className="bug-preview">
-        <p className="title">{bug.title}</p>
-        <p>Severity: <span>{bug.severity}</span></p>
+export function BugPreview({ bug }) {
+  return (
+    <article className='bug-preview'>
+      <p className='title'>{bug.title}</p>
+      <p>
+        description: <span>{bug.description}</span>
+      </p>
+      <p>
+        Severity: <span>{bug.severity}</span>
+      </p>
     </article>
+  )
 }

26209609123cc3ee565cfeaa8e268bff92437a35
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2025-04-01 13:57:54 +0300

    API routes were added on the server side for user management and authentication, including login, signup, logout, and retrieving user details via REST API. Corresponding services were implemented on both the server and client sides to handle these actions.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import bodyParser from 'body-parser'
 
 import { bugService } from './services/bug.service.server-side.js'
 import { loggerService } from './services/logger.service.js'
+import { userService } from './services/user.service.js'
+import { authService } from './services/auth.service.js'
 
 const app = express()
 
@@ -168,6 +170,63 @@ app.delete('/api/bug/:bugId', (req, res) => {
       loggerService.error('Cannot remove bug', err)
       res.status(500).send('Cannot remove bug')
     })
+})
+
+app.get('/api/user', (req, res) => {
+  userService
+    .query()
+    .then((users) => res.send(users))
+    .catch((err) => {
+      loggerService.error('Cannot load users', err)
+      res.status(400).send('Cannot load users')
+    })
+})
+
+app.get('/api/user/:userId', (req, res) => {
+  const { userId } = req.params
+
+  userService
+    .getById(userId)
+    .then((user) => res.send(user))
+    .catch((err) => {
+      loggerService.error('Cannot load user', err)
+      res.status(400).send('Cannot load user')
+    })
+})
+
+app.post('/api/auth/login', (req, res) => {
+  const credentials = req.body
+
+  authService
+    .checkLogin(credentials)
+    .then((user) => {
+      const loginToken = authService.getLoginToken(user)
+      res.cookie('loginToken', loginToken)
+      res.send(user)
+    })
+    .catch(() => res.status(404).send('Invalid Credentials'))
+})
+
+app.post('/api/auth/signup', (req, res) => {
+  const credentials = req.body
+
+  userService
+    .add(credentials)
+    .then((user) => {
+      if (user) {
+        const loginToken = authService.getLoginToken(user)
+        res.cookie('loginToken', loginToken)
+        res.send(user)
+      } else {
+        res.status(400).send('Cannot signup')
+      }
+    })
+    .catch((err) => res.status(400).send('Username taken.'))
+})
+
+app.post('/api/auth/logout', (req, res) => {
+  res.clearCookie('loginToken')
+  res.send('logged-out!')
 })
 
 app.get('/', (req, res) => {
